7e6238bcbf1d0f9530cbbae29c2999771f3a7270
m4mbt <mariammrabet12@gmail.com>
2025-06-09 22:33:54 +0200

    Update my_app.py

---
+++ my_app.py
@@ -0,0 +1,22 @@
+# instr.py
+txt_title = 'Health Check'
+win_x, win_y = 200, 100
+win_width, win_height = 1000, 600
+
+txt_hello = 'Welcome to the Health Check program!'
+txt_instruction = (
+    "This program helps you use the Rufier test to make a first evaluation of your health.\n"
+    "In 45 seconds, do 30 squats and measure your heart rate during the final 15 seconds of the first minute\n"
+    "of recovery.\n"
+    "Note: If you feel unwell (dizzy or faint), stop immediately."
+)
+
+txt_next = 'Start Test'
+txt_index = 'Rufier Index: '
+txt_workheart = 'Heart Function: '
+
+txt_res1 = 'very low. Please see a doctor as soon as possible!'
+txt_res2 = 'fair. We recommend you consult a doctor!'
+txt_res3 = 'average. You might want to consider getting checked by a doctor.'
+txt_res4 = 'good'
+txt_res5 = 'excellent'
